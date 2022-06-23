f50ff80b79ee94a6da64f8731d8132639794e33a
Romuald Conty <romuald@opus-codium.fr>
2022-06-23 20:42:06 +0200

    Data structurer: Handle RPC errors

--- lib/choria/colt/data_structurer.rb
+++ lib/choria/colt/data_structurer.rb
@@ -4,14 +4,25 @@ module Choria
       def self.structure(res) # rubocop:disable Metrics/AbcSize
         return res unless [0, 1].include? res[:statuscode]
 
+        # If data is empty, status message is an RPC error
+        if res[:data].empty?
+          res[:result] = {
+            _error: {
+              kind: 'choria/rpc',
+              msg: res[:statusmsg],
+            },
+          }
+          return res
+        end
+
         # data.stdout seems to always be JSON, so parse it once.
-        res[:result] = JSON.parse res[:data][:stdout]
+        res[:result] = JSON.parse res[:data][:stdout] unless res.dig(:data, :stdout).nil?
         res[:data].delete :stdout
 
         # On one side, data.stderr is filled by the remote execution stderr.
         # On the other side, error description is in JSON (ie. '_error')
         # So merge data.stderr in '_error'.'details'
-        unless res[:data][:stderr].empty?
+        unless res.dig(:data, :stderr).nil? || res[:data][:stderr].empty?
           raise NotImplementedError, 'What to do when res[:data][:stderr] contains something?' if res[:result]['_error'].empty?
 
           res[:result]['_error']['details'].merge!({ 'stderr' => res[:data][:stderr].split("\n") })
@@ -19,7 +30,7 @@ module Choria
         res[:data].delete :stderr
 
         # Convert '_output' (ie. stdout) lines into array
-        res[:result]['_output'] = res[:result]['_output'].split("\n") unless res[:result]['_output'].nil?
+        res[:result]['_output'] = res[:result]['_output'].split("\n") unless res.dig(:result, '_output').nil?
 
         res
       end
