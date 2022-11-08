fc7cff026984a47373cf4f7fb0d54f013ec0f3ee
Romuald Conty <romuald@opus-codium.fr>
2022-11-08 11:00:12 +0100

    Spec/Formatter: Use raw responses

--- spec/cli/formatter_spec.rb
+++ spec/cli/formatter_spec.rb
@@ -3,22 +3,33 @@ require 'choria/colt/cli/formatter'
 require 'active_support'
 require 'active_support/core_ext/hash/indifferent_access'
 
-def load_from_result_file(file)
-  JSON.parse(File.read(File.join(__dir__, '..', 'fixtures', "#{file}.json"))).first.with_indifferent_access
-end
-
 RSpec.describe Choria::Colt::CLI::Formatter do
   let(:formatter) { Choria::Colt::CLI::Formatter.new(colored: false) }
+
+  let(:result_set) do
+    directory = result_set_name
+    result_set = Choria::Orchestrator::Task::ResultSet.new on_result: nil
+
+    path = File.join(__dir__, '..', 'fixtures', 'orchestrator', 'task', 'result_sets', directory)
+    Dir.glob("#{path}/*.json") do |filename|
+      result_set.integrate_result JSON.parse(File.read(filename)).with_indifferent_access
+    end
+
+    result_set
+  end
+
+  let(:first_result) { result_set.results.first }
 
   context 'using the result of `exec` task run' do
     context 'when command ran successfully with output' do
       # "/bin/true --version"
-      let(:rpc_result) { load_from_result_file 'bin_true__version' }
-      describe '#process_result' do
+      let(:result_set_name) { 'exec__bin_true__version' }
+
+      describe '#format formats the result' do
         it 'formats a successful result' do
-          expect(formatter.format(rpc_result).to_s).to eq(
+          expect(formatter.format(first_result).to_s).to eq(
             <<~OUTPUT.chomp
-              √ vm012345.example.com                                        duration: 0.08s
+              √ vm001.example.com                                           duration: 0.14s
                 true (GNU coreutils) 8.32
                 Copyright (C) 2020 Free Software Foundation, Inc.
                 License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
@@ -34,21 +45,22 @@ RSpec.describe Choria::Colt::CLI::Formatter do
 
     context 'when command ran with error with output' do
       # "/bin/false --version"
-      let(:rpc_result) { load_from_result_file 'bin_false__version' }
-      describe '#process_result' do
+      let(:result_set_name) { 'exec__bin_false__version' }
+
+      describe '#format formats the result' do
         it 'format an error result' do
-          expect(formatter.format(rpc_result).to_s).to eq(
+          expect(formatter.format(first_result).to_s).to eq(
             # rubocop:disable Layout/TrailingWhitespace
             <<~OUTPUT.chomp
-              ⨯ vm012345.example.com                                        duration: 0.17s
+              ⨯ vm001.example.com                                           duration: 0.17s
                 choria.tasks/task-error: The task errored with a code 1
                   details: {
                     "exitcode": 1
                   }
                 
                 output:
-                false (GNU coreutils) 8.30
-                Copyright (C) 2018 Free Software Foundation, Inc.
+                false (GNU coreutils) 8.32
+                Copyright (C) 2020 Free Software Foundation, Inc.
                 License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
                 This is free software: you are free to change and redistribute it.
                 There is NO WARRANTY, to the extent permitted by law.
@@ -62,12 +74,13 @@ RSpec.describe Choria::Colt::CLI::Formatter do
     end
 
     context 'when RPC failed with error' do
-      let(:rpc_result) { load_from_result_file 'rpc_error' }
-      describe '#process_result' do
+      let(:result_set_name) { 'rpc_error' }
+
+      describe '#format formats the result' do
         it 'format an error result' do
-          expect(formatter.format(rpc_result).to_s).to eq(
+          expect(formatter.format(first_result).to_s).to eq(
             <<~OUTPUT.chomp
-              ⨯ vm012345.example.com
+              ⨯ vm001.example.com
                 RPC error (5): Task exec is not available or does not match the specification, please download it
             OUTPUT
           )
